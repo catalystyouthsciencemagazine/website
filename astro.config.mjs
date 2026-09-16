dc75a482248f378ac22ff8b19649388333a79204
godmothersfire <znotfireman@gmail.com>
2026-09-16 14:55:41 +0700

    fix(ci): change base page

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import icon from "astro-icon";
 // https://astro.build/config
 export default defineConfig({
 	site: "https://catalystyouthsciencemagazine.github.io",
-	base: "/catalystyouth.org",
+	base: "/website",
 
 	vite: {
 		plugins: [tailwindcss()],
