5b14af8d0c4f293984ee863d3e056dbfa4acf193
godmothersfire <znotfireman@gmail.com>
2026-09-16 14:49:16 +0700

    fix(ci): update site base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://rniraclefire.github.io",
+	site: "https://catalystyouthsciencemagazine.github.io",
 	base: "/catalystyouth.org",
 
 	vite: {
